04af1a1b799a76d96c696b7d26ea76c0f3a0b7b9
Ji-Ping Shen <ji-ping.shen@relexsolutions.com>
2021-07-09 15:21:42 +0300

    Fix inline problem with go 1.16

--- output/fluentdforward/eventtime.go
+++ output/fluentdforward/eventtime.go
@@ -8,7 +8,7 @@ import (
 
 // EncodeEventTime encodes a float64 as EventTime
 func EncodeEventTime(buffer []byte, start int, value time.Time) int {
-	pos := fastmsgpack.EncodeExtHeader(buffer, start, 0, 8)
+	pos := fastmsgpack.EncodeExtHeader8(buffer, start, 0)
 	pos = fastmsgpack.Write4(buffer, pos, uint32(value.Unix()))
 	return fastmsgpack.Write4(buffer, pos, uint32(value.Nanosecond()))
 }
