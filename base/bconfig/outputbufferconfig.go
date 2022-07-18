88681329582fe68a0bb680fd643282975c4eb598
Dmitry Andreev <dmitry.andreev@relexsolutions.com>
2022-07-18 12:09:15 +0300

    fix: check for outputBufferPair name duplication

--- base/bconfig/outputbufferconfig.go
+++ base/bconfig/outputbufferconfig.go
@@ -1,5 +1,11 @@
 package bconfig
 
+import (
+	"fmt"
+
+	"github.com/relex/slog-agent/base"
+)
+
 // OutputBufferConfig configures buffer and output settings
 
 type OutputBufferConfig struct {
@@ -7,4 +13,16 @@ type OutputBufferConfig struct {
 	Name         string                          `yaml:"name"`
 	BufferConfig ConfigHolder[ChunkBufferConfig] `yaml:"buffer"`
 	OutputConfig ConfigHolder[LogOutputConfig]   `yaml:"output"`
+}
+
+func (cfg OutputBufferConfig) VerifyConfig(schema base.LogSchema) error {
+	if err := cfg.BufferConfig.Value.VerifyConfig(); err != nil {
+		return fmt.Errorf("buffer config validation error: %w", err)
+	}
+
+	if err := cfg.OutputConfig.Value.VerifyConfig(schema); err != nil {
+		return fmt.Errorf("output config validation error: %w", err)
+	}
+
+	return nil
 }
