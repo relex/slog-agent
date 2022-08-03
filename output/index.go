d1a69f0e1dcba0aca2c9ce7db68bda42d60d957e
Dmitry Andreev <dmitry.andreev@relexsolutions.com>
2022-08-03 15:55:29 +0300

    feat: add datadog input

--- output/index.go
+++ output/index.go
@@ -3,11 +3,13 @@ package output
 
 import (
 	"github.com/relex/slog-agent/base/bconfig"
+	"github.com/relex/slog-agent/output/datadog"
 	"github.com/relex/slog-agent/output/fluentdforward"
 )
 
 func init() {
 	bconfig.RegisterConfigConstructors(bconfig.LogOutputConfigCreatorTable{
+		"datadog":        func() bconfig.LogOutputConfig { return &datadog.Config{} },
 		"fluentdForward": func() bconfig.LogOutputConfig { return &fluentdforward.Config{} },
 	})
 }
